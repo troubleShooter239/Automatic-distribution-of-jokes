c870b27b2f31a8f5e16c1e27d31367b90520fb79
fcKurCode <morgun2282@gmail.com>
2023-02-26 23:28:34 +0300

    Update Autojokes.py

--- Autojokes.py
+++ Autojokes.py
@@ -2,36 +2,53 @@
 
 from telethon import TelegramClient
 from bs4 import BeautifulSoup
-import requests, lxml, random
+from random import choice
+from requests import get
+
+
+def get_recipient():
+    """Функция ввода имени пользователя получателя"""
+    recipient = ''
+    while recipient == '':
+        recipient = input('Введите username (пример: @VovkaPutin228): ')
+
+    return recipient
 
 
 def scrab():
     """Функция скрабинга страницы сайта"""
-    page = requests.get('https://anekdoty.ru/pro-hoholov/page/' + str(1) + '/')  # пока берем информацию с первой стр
-    # с этого сайта можно не только про хохлов анеки кидать
-    page = page.text  # преобразование в текст
-    soup = BeautifulSoup(page, 'lxml')  # делаем скраблинг страницы при помощи lxml
-    page_p = soup.find_all('p')  # находим на странице анеки
+    page = ''
+    for i in range(1, 5):  # берем информацию со всех страниц
+        page += get('https://anekdoty.ru/pro-hoholov/page/' + str(i) + '/').text
 
-    return random.choice(page_p).text # возвращаем рандомный анек
+        page = BeautifulSoup(page, 'lxml')  # делаем скрабинг страницы при помощи lxml
+        page = page.find_all('p')  # находим на странице анеки
+
+        return choice(page).text  # возвращаем рандомный анек
 
 
-def message(internal_api_id, internal_api_hash, internal_recipient):
+def message(api_id, api_hash, recipient):
     """Функция отправки сообщений"""
-    with TelegramClient('AutoanecXD', internal_api_id, internal_api_hash) as client:
-        client.loop.run_until_complete(client.send_message(internal_recipient, scrab())) # зацикливаем пока не отправиться сообщение
-    print('Анек отправлен XD')
+    try:
+        while True:  # бесконечный цикл
+            input('Нажмите клавишу ввода для отправки анекдота.')  # ждем нажатия клавиши
+            with TelegramClient('AutoanecXD', api_id, api_hash) as client:  # отправляем сообщение пользователю
+                client.loop.run_until_complete(client.send_message(recipient, scrab()))
+            print('Анек отправлен XD')
+
+    except KeyboardInterrupt:
+        print('Остановлено вами.')
+
+    except ValueError:
+        print('Ошибка отправки адресату.')
 
 
 def main():
     """Главная функция программы"""
-    # поменять айди и хэш на рандомные чтобы меня не взломали нахуй
-    api_id = 2353286  # тг api id
-    api_hash = 'a37d3089e20a5cd6e6743b78d59b0b74'  # тг api hash
-    recipient = 'me'  # получатель анеков
-
-    message(api_id, api_hash, recipient)
+    api_id = 69696969  # тг api id
+    api_hash = 'а1337абоба228339'  # тг api hash
+    message(api_id, api_hash, get_recipient())
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # точка старта программы
     main()
